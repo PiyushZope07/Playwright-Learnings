48c845e7dbfff6c3872ec1d61275f0fbd8b1b7ff
PiyushZope07 <piyushzope8891@gmail.com>
2026-02-08 12:23:30 +0530

    Handling Input Fields

---
+++ tests/InputBoxes.spec.js
@@ -0,0 +1,20 @@
+import {test,expect} from '@playwright/test';
+
+test('Input Box',async({page})=>{
+    await page.goto("https://www.testmuai.com/selenium-playground/simple-form-demo/")
+
+    await page.getByPlaceholder("Please enter first value").fill("10");
+
+    await page.locator("#sum2").fill("20");
+
+    await expect(page.getByRole('button', { name: 'Get Sum' })).toBeVisible();
+
+    await page.getByRole('button', { name: 'Get Sum' }).click();
+
+    await page.waitForTimeout(2000);
+
+    await expect(page.locator("#addmessage")).toBeVisible();
+
+    await expect(page.locator("#addmessage")).toHaveText("30")
+
+})
